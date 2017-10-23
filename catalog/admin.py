1de418aef9e7bc1973d424e5e09efc5cadf9503c
MANOJ PATRA <patra.manoj0@gmail.com>
2017-10-23 17:03:13 +0530

    Add BookInline to AuthorAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,10 +8,18 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
 #  Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [
+        BookInline
+    ]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
