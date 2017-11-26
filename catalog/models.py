669a99e920ed55b3018ac94a357c07e5a53220f2
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-26 14:50:34 +0530

    Code refactor

--- catalog/models.py
+++ catalog/models.py
@@ -10,8 +10,10 @@ class Genre(models.Model):
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
-    name = models.CharField(max_length=200,
-                            help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
 
     def __str__(self):
         """
@@ -24,7 +26,10 @@ class Language(models.Model):
     """
     Model representing a Language (e.g. English, French, Japanese, etc.)
     """
-    name = models.CharField(max_length=200, help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)"
+    )
 
     def __str__(self):
         """
@@ -50,7 +55,7 @@ class Author(models.Model):
 
     def __str__(self):
         """
-        String for representing the Model object.
+        String for representing the Author object.
         """
         return "{0} ({1})".format(self.last_name, self.first_name)
 
@@ -62,7 +67,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN', max_length=13, help_text="13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>")
+
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        help_text="13 Character <a href='https://www.isbn-international.org/content/what-isbn'>ISBN number</a>"
+    )
+
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
@@ -92,7 +103,12 @@ class BookInstance(models.Model):
     """
     Model representing a specific copy of a book (i.e. that can be borrowed from the library).
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Unique ID for this particular book across whole library"
+    )
+
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -104,10 +120,17 @@ class BookInstance(models.Model):
         ('r', 'Reserved'),
     )
 
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text="Book availability")
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text="Book availability"
+    )
 
     class Meta:
-        # order records by due_date when they are returned in a query.
+        # Define an order
+        # Order records by due_date when they are returned in a query.
         ordering = ["due_back"]
 
     def __str__(self):
