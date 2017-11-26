be5392d14b3d2eb33874542fcc63df551a31bd47
MANOJ PATRA <patra.manoj0@gmail.com>
2017-11-26 14:47:34 +0530

    Add list, detail view for books and authors

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 
@@ -13,6 +14,11 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()    # The 'all()' is implied by default.
+    genres = Genre.objects.count()
+
+    # Number of visits to this view as counted in the session variable
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     return render(request,
                   'index.html',
@@ -20,5 +26,47 @@ def index(request):
                       'num_books': num_books,
                       'num_instances': num_instances,
                       'num_instances_available': num_instances_available,
-                      'num_authors': num_authors
+                      'num_authors': num_authors,
+                      'genres': genres,
+                      'num_visits': num_visits
                   })
+
+
+class BookListView(generic.ListView):
+    """
+    View function to list all books
+    """
+    model = Book
+    # Our own name for the list as a template variable
+    context_object_name = 'book_list'
+    template_name = 'catalog/book_list.html'
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    """
+    View function to display a book's details
+    """
+    model = Book
+    context_object_name = 'book'
+    template_name = 'catalog/book_detail.html'
+
+
+class AuthorListView(generic.ListView):
+    """
+    View function to display an author's details
+    """
+    model = Author
+    context_object_name = 'author_list'
+    template_name = 'catalog/author_list.html'
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """
+    View function to display an author's details
+    """
+    model = Author
+    context_object_name = 'author'
+    template_name = 'catalog/author-detail.html'
+
